70f0f7f51f7ff27b4cd88b7a4669de342e1059d4
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-15 15:21:05 -0700

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require("dotenv").config()
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
