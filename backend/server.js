a1d186592ce8d84bf8bac27948e5e22735d0a887
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-18 00:22:24 -0700

    feat: add errorHandler back and run formatter

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const bookRoutes = require("./routes/bookRoutes");
-
 
 require("dotenv").config();
 
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/books", bookRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
