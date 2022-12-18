6e7f775862d004299940ef5e9686495089017b95
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-17 23:06:50 -0700

    chore: clean up and reorganize lines

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-const { errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
-
 const connectDB = require("./config/db");
 const bookRoutes = require("./routes/bookRoutes");
+
+
+require("dotenv").config();
 
 connectDB();
 
@@ -14,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/books", bookRoutes);
-
-app.use(errorHandler)
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
