05a9732c3d6455d03c9030da87d4ab9266f6abe9
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-18 00:21:51 -0700

    feat: add a route to get a book

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
 	getAllBooks,
+	getBook,
 	addBook,
 	updateBook,
 	deleteBook,
 } = require("../controllers/bookController");
 
 router.get("/", getAllBooks);
+router.get('/:id', getBook)
 router.post("/", addBook);
 router.put("/:id", updateBook);
 router.delete("/:id", deleteBook);
