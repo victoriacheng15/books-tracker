058ef8e528ce6bac729dae37aab147aa63c84275
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-17 23:06:27 -0700

    chore: add strictQuery option

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
+		mongoose.set("strictQuery", false);
 		const db = await mongoose.connect(process.env.MONGO_URI);
 
 		console.log(`MongoDB connected: ${db.connection.host}`);
